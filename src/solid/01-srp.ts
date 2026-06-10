838d4fd069276fc95f5f5435f9a4fe642088c046
AlexDaniel593 <dalexis203@gmail.com>
2026-06-10 07:58:59 -0500

    refactor: inyectar UserService y Mailer en el constructor de UserBloc

--- src/solid/01-srp.ts
+++ src/solid/01-srp.ts
@@ -1,17 +1,34 @@
 import { SubscriptionBloc } from './subscription-bloc';
 import { UserService } from './user-service';
 import { Mailer } from './mailer';
+import { User } from './user';
 
 class UserBloc {
+    private userService: UserService;
+    private mailer: Mailer;
+
+    constructor( userService: UserService, mailer: Mailer ) {
+        this.userService = userService;
+        this.mailer = mailer;
+    }
+
+    getUser( id: number ) {
+        return this.userService.loadUser( id );
+    }
+
+    createUser( user: User ) {
+        this.userService.saveUser( user );
+        this.mailer.sendEmail( user.name, 'Bienvenido' );
+    }
 
 }
 
-const userBloc = new UserBloc();
 const userService = new UserService();
 const mailer = new Mailer();
 const subscriptionBloc = new SubscriptionBloc();
 
-userService.loadUser(10);
-userService.saveUser({ id: 10, name: 'Fernando' });
-mailer.sendEmail('fernando@google.com', 'Bienvenido');
+const userBloc = new UserBloc( userService, mailer );
+
+userBloc.getUser(10);
+userBloc.createUser({ id: 10, name: 'Fernando' });
 subscriptionBloc.onAddSubscription(1234);
